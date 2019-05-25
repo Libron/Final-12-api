dfa3448533473ec9a2a9b6ddda68ccd1817e4d20
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-25 15:46:24 +0600

    users route added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const config = require('./config');
 const gallery = require('./app/gallery');
+const users = require('./app/users');
 
 const app = express();
 
@@ -16,6 +17,7 @@ const port = 8000;
 mongoose.connect(config.dbUrl, config.mongoOptions).then(() => {
     app.listen(port, () => {
         app.use('/gallery', gallery);
+        app.use('/users', users);
         console.log(`Server started on ${port} port`);
     });
 });
